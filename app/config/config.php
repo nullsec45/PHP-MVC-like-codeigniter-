029fd42f5a18fd311d4719326003d3b542b999bd
ramarff <rama21010ti@student.nurulfikri.ac.id>
2022-09-26 09:48:26 +0700

    php mvc

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+define("BASEURL","http://localhost/mvc/public");
+
+
+// DB
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASS","");
+define("DB_NAME","mvc_mahasiswa");
+ ?>
